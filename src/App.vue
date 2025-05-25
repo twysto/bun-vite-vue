da25fa3823395714d0e4c3aa9736528e9ab5a011
twysto <twyst.tech@disroot.org>
2025-05-25 04:31:59 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,13 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://bun.sh" target="_blank">
-      <img src="bun.svg" class="logo" alt="Bun logo" />
+      <img src="./img/svg/bun.svg" class="logo" alt="Bun logo" />
     </a>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="./assets/vite.svg" class="logo vite" alt="Vite logo" />
+      <img src="./img/svg/vite.svg" class="logo vite" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./img/svg/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Bun + Vite + Vue" />
@@ -23,12 +23,15 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #fbf0dfaa);
 }
+
 .logo.vite:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
